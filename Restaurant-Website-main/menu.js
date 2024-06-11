c41c2911f927f572d435c5816dfb516bbc45c519
Sabri Jonuzi <sj61243@ubt-uni.net>
2024-06-11 17:57:56 +0200

    first commit

---
+++ Restaurant-Website-main/menu.js
@@ -0,0 +1,18 @@
+// scripts.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('nav ul li a');
+
+    links.forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            const targetId = link.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            window.scrollTo({
+                top: targetElement.offsetTop,
+                behavior: 'smooth'
+            });
+        });
+    });
+});
