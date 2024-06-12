bb1d5ac142ec8495944ed69e3d5dd12c8a25a30d
Sabri Jonuzi <sj61243@ubt-uni.net>
2024-06-12 21:39:49 +0200

    .

--- scripts.js
+++ scripts.js
@@ -1,16 +1,34 @@
 $(document).ready(function() {
+    // Fshij preloader-in kur ngarkohet faqja
     $(window).on('load', function() {
         $('#preloader').fadeOut('slow', function() {
             $(this).remove();
         });
-    }
-);
+    });
 
-});
-$(document).ready(function () {
+    // Kontrollo slidimin e carousel-it dhe ndryshimin e caption-it
     $('#carouselExampleIndicators').on('slid.bs.carousel', function (e) {
         var index = $(e.relatedTarget).index();
         $('.carousel-caption-text').removeClass('active');
         $('.carousel-caption-text').eq(index).addClass('active');
+    });
+
+    // Inicializo Slider-in me Swiper
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: true,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
     });
 });
